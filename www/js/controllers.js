de90d5e733c8d0786d3a8386d21aba83f5140e5a
Andre Felizardo <andreluis.felizardo@outlook.com>
2017-05-07 18:05:08 -0300

    Criação do app seja grato

---
+++ www/js/controllers.js
@@ -0,0 +1,26 @@
+angular.module('app.controllers', [])
+  
+.controller('sejaGratoCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams) {
+
+
+}])
+   
+.controller('sobreCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams) {
+
+
+}])
+   
+.controller('menuCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams) {
+
+
+}])
+ 
---
+++ www/js/routes.js
@@ -0,0 +1,43 @@
+angular.module('app.routes', [])
+
+.config(function($stateProvider, $urlRouterProvider) {
+
+  // Ionic uses AngularUI Router which uses the concept of states
+  // Learn more here: https://github.com/angular-ui/ui-router
+  // Set up the various states which the app can be in.
+  // Each state's controller can be found in controllers.js
+  $stateProvider
+    
+  
+
+      .state('menu.sejaGrato', {
+    url: '/home',
+    views: {
+      'side-menu21': {
+        templateUrl: 'templates/sejaGrato.html',
+        controller: 'sejaGratoCtrl'
+      }
+    }
+  })
+
+  .state('menu.sobre', {
+    url: '/sobre',
+    views: {
+      'side-menu21': {
+        templateUrl: 'templates/sobre.html',
+        controller: 'sobreCtrl'
+      }
+    }
+  })
+
+  .state('menu', {
+    url: '/side-menu21',
+    templateUrl: 'templates/menu.html',
+    controller: 'menuCtrl'
+  })
+
+$urlRouterProvider.otherwise('/side-menu21/home')
+
+  
+
+});
