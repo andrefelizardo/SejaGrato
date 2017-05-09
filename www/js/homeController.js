c598be0ec266058d5978b6acfb0a60a7d8261195
Alllen <andre.felizardo@ctallen.com.br>
2017-05-09 15:44:12 -0300

    localStorage funcionando

--- www/js/homeController.js
+++ www/js/homeController.js
@@ -1,6 +1,6 @@
 angular.module('sejaGrato').controller('HomeController', function($scope){
 	$scope.lista = [];
-
+	$scope.dadosLocal = '';
 
 	$scope.salvarTexto = function() {
 				// data atual
@@ -10,6 +10,20 @@ angular.module('sejaGrato').controller('HomeController', function($scope){
 				var ano = data.getFullYear();
 				$scope.lista.data = [dia, mes, ano].join('/');
 				$scope.lista.push({texto: $scope.lista.texto, data: $scope.lista.data});
+				var listaJson = angular.toJson($scope.lista);
+				localStorage.setItem('mensagensSejaGrato', listaJson);
 				$scope.lista.texto = '';
 			}
+
+			$scope.pageLoad = function() {
+				var listaSalva = localStorage.getItem('mensagensSejaGrato');
+				if(listaSalva != null) {
+					$scope.lista = angular.fromJson(listaSalva);
+					$scope.dadosLocal = true;
+				} else {
+					$scope.dadosLocal = false;
+				}
+			}
+
+			$scope.pageLoad();
 		});
