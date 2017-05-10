93ef12c50013483dbf44826bae3cbe10e17feb41
Andre Felizardo <andreluis.felizardo@outlook.com>
2017-05-10 07:21:54 -0300

    inicio validacao form

--- www/js/criarContaController.js
+++ www/js/criarContaController.js
@@ -1,7 +1,15 @@
-angular.module('sejaGrato').controller('criarContaController', function($scope) {
+angular.module('sejaGrato').controller('criarContaController', function($scope, $ionicPopup, $timeout) {
 	$scope.conta = [];
 
 	$scope.criarConta = function() {
-		
+		if(criarContaForm.email.$error.required) {
+			console.log('campo obrigatório');
+		}
+		if($scope.conta.senha != $scope.conta.confirmaSenha) {
+			var alertPopup = $ionicPopup.alert({
+				title: 'Dados Incorretos',
+				template: 'Confirmação de senha deve ser igual a senha.'
+			});
+		}
 	}
 });
